641b850cc123b520f20fa897f59dda5bdabf8c67
JosueVanegas <josuerocha461@gmail.com>
2023-12-29 19:53:05 -0600

    last update

--- weatherapp/src/components/WeatherCard.jsx
+++ weatherapp/src/components/WeatherCard.jsx
@@ -1,24 +1,47 @@
 import { useState } from "react";
 
 const API_KEY = '2f5adc0a2c126d3cad6a224c330d7d47';
+
 const WeatherCard = () => {
   const [weatherData, setWeatherData] = useState({});
   const [search, setSearch] = useState('');
+  const [image, setImage] = useState('');
 
   const handleSubmit = async () => {
     try {
-      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search.trim()}&appid=${API_KEY}`;
+      const url = `http://api.openweathermap.org/data/2.5/weather?q=${search.toLowerCase().trim()}&appid=${API_KEY}`;
       const res = await fetch(url);
       const data = await res.json();
       if (!data || data.cod === '404') {
         alert('Country/city not found');
         return;
-      }else{
+      } else {
         setWeatherData(data);
+        updateImage(data);
       }
-      
+
     } catch (err) {
       console.error(err);
+    }
+  };
+
+  const updateImage = (data) => {
+    const mainWeather = data.weather && data.weather[0] && data.weather[0].main;
+    switch (mainWeather) {
+      case 'Clear':
+        setImage('https://cdn-icons-png.flaticon.com/128/169/169367.png');
+        break;
+      case 'Clouds':
+        setImage('https://cdn-icons-png.flaticon.com/128/11742/11742566.png');
+        break;
+      case 'Rain':
+        setImage('https://cdn-icons-png.flaticon.com/128/1164/1164945.png');
+        break;
+      case 'Storm': setImage('https://cdn-icons-png.flaticon.com/128/1959/1959334.png')
+        break;
+      default:
+        setImage('URL_POR_DEFECTO_O_PARA_OTROS_CASOS');
+        break;
     }
   };
 
@@ -42,24 +65,47 @@ const WeatherCard = () => {
             value={search}
             onChange={(e) => setSearch(e.target.value)}
           />
-          <button className="bg-slate-800 p-2 m-10 mt-0 text-white rounded-xl hover:bg-slate-400 hover:text-slate-800"
-            type="submit" onClick={handleSubmit}>Search</button>
+          <button
+            className="bg-slate-800 p-2 m-10 mt-0 text-white rounded-xl hover:bg-slate-400 hover:text-slate-800"
+            type="button"  // Cambiado de 'submit' a 'button'
+            onClick={handleSubmit}
+          >
+            Search
+          </button>
         </div>
         <div>
-          {weatherData && weatherData.weather && weatherData.main &&(
-            <div className="m-5">
-              {
-                console.log(weatherData)
-              }
-              <div className="pb-10">
-                <h1 className="text-3xl pb-10">{`${weatherData.name} | ${weatherData.sys && weatherData.sys.country}`}</h1>
-                <h2 className="text-lg text-left pb-5">{`Weather: ${mainWeather} | ${description}`}</h2>
-                <h3 className="text-lg text-left">{`Humidity: ${humidity !== undefined ? humidity + ' %' : 'N/A'}`}</h3>
-                <h3 className="text-lg text-left">{`Temp: ${temperatureCelsius !== undefined ? Math.round(temperatureCelsius - 273.15) + ' °C' : 'N/A'}`}</h3>
-                <h3 className="text-lg text-left">{`Feels like: ${feelsLikeCelsius !== undefined ? Math.round(feelsLikeCelsius - 273.15) + ' °C' : 'N/A'}`}</h3>
+          <div>
+            {weatherData && weatherData.weather && weatherData.main && (
+              <div className="m-5 bg-white p-10">
+                <div>
+                  {image && (
+                    <div>
+                      <img src={image} alt="" />
+                    </div>
+                  )}
+                </div>
+                <div className="pb-10">
+                  <h1 className="text-3xl pb-10">{`${weatherData.name} | ${
+                    weatherData.sys && weatherData.sys.country
+                  }`}</h1>
+                  <h2 className="text-lg text-left pb-5">{`Weather: ${mainWeather} | ${description} `}</h2>
+                  <h3 className="text-lg text-left">{`Humidity: ${
+                    humidity !== undefined ? humidity + ' %' : 'N/A'
+                  }`}</h3>
+                  <h3 className="text-lg text-left">{`Temp: ${
+                    temperatureCelsius !== undefined
+                      ? Math.round(temperatureCelsius - 273.15) + ' °C'
+                      : 'N/A'
+                  }`}</h3>
+                  <h3 className="text-lg text-left">{`Feels like: ${
+                    feelsLikeCelsius !== undefined
+                      ? Math.round(feelsLikeCelsius - 273.15) + ' °C'
+                      : 'N/A'
+                  }`}</h3>
+                </div>
               </div>
-            </div>
-          )}
+            )}
+          </div>
         </div>
       </div>
     </div>
